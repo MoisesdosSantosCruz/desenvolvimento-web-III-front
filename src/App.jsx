9a7c9c5bfe17876be32591260b6a461072d05235
MoisesdosSantosCruz <moises.cruz01@aluno.cps.sp.gov.br>
2026-09-02 10:12:52 -0300

    Implementa navegação e gerenciamento de clientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
+import { Routes, Route } from 'react-router'
 import './App.css'
 import Cabecalho from './components/Cabecalho';
 import CardModulo from './components/CardMordulo';
+import Clientes from './pages/Clientes'
+import ListaClientes from './pages/ListaClientes'
+import CadastroCliente from './pages/CadastroClientes';
+
 
 function App() {
   const [mostrarModulos, setMostrarModulos] = useState(true)
 
-  const modulos = [
+  const [modulos] = useState([
     {
       id: 1,
       titulo: 'Gerenciamento de Produtos',
@@ -16,6 +21,7 @@ function App() {
       id: 2,
       titulo: 'Gerenciamento de Clientes',
       descricao: 'Cadastre e consulte os clientes da empresa.',
+      rota: '/clientes',
     },
     {
       id: 3,
@@ -27,37 +33,58 @@ function App() {
       titulo: 'Gerenciamento de Vendas',
       descricao: 'Registre e consulte as vendas realizadas.',
     }
-  ]
+  ])
 
   return (
-    <div className="aplicacao">
-      <Cabecalho />
-      <main className="conteudo-principal">
-        <p className="introducao">
-          Aplicação desenvolvida nas disciplinas de Desenvolvimento Web III e
-          Tópicos de Programação II.
-        </p>
-        <button
-          type="button"
-          className="botao-alternar"
-          onClick={() => setMostrarModulos(!mostrarModulos)}
-        >
-          {mostrarModulos ? 'Ocultar módulos' : 'Exibir módulos'}
-        </button>
-        {mostrarModulos && (
-        <section className="modulos">
-          {modulos.map((modulo) => (
-            <CardModulo
-              key={modulo.id}
-              titulo={modulo.titulo}
-              descricao={modulo.descricao}
-            />
-          ))}
-        </section>
-      )}
-      </main>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element = {
+          <div className="aplicacao">
+            <Cabecalho />
+            <main className="conteudo-principal">
+              <p className="introducao">
+                Aplicação desenvolvida nas disciplinas de Desenvolvimento Web III e
+                Tópicos de Programação II.
+              </p>
+              <button
+                type="button"
+                className="botao-alternar"
+                onClick={() => setMostrarModulos(!mostrarModulos)}
+              >
+                {mostrarModulos ? 'Ocultar módulos' : 'Exibir módulos'}
+              </button>
+              {mostrarModulos && (
+              <section className="modulos">
+                {modulos.map((modulo) => (
+                  <CardModulo
+                    key={modulo.id}
+                    titulo={modulo.titulo}
+                    descricao={modulo.descricao}
+                    rota = {modulo.rota}
+                  />
+                ))}
+              </section>
+            )}
+            </main>
+          </div>
+        }
+      />
+        <Route 
+          path="/clientes" 
+          element={<Clientes />} 
+        />
+        
+        <Route
+          path="/clientes/listar"
+          element={<ListaClientes />}
+        />
+        <Route
+        path="/clientes/cadastrar"
+        element ={<CadastroCliente />}
+      />
 
+    </Routes>
   )
 }
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'//Avisa quando ter algum erro no código
 import { createRoot } from 'react-dom/client'//Diz onde é a raiz na página html
+import { BrowserRouter } from 'react-router'
 import './index.css' //importa o estilo da index.css
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <App /> {/*Onde a renderização vai aparecer*/} 
+    </BrowserRouter>
   </StrictMode>,
 )
